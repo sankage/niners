dcde2441b2c67caabb28d733d806c6d8422005ff
David Briggs <godamonra@gmail.com>
2020-01-11 21:52:58 -0500

    Update rails to 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_020519) do
     t.boolean "rider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "on_standby"
+    t.boolean "on_standby", default: false
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_players_on_deleted_at"
   end
